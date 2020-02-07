d212667e469a0efa9cad6629da5d7954a413cde2
Jay R Bolton <jayrbolton@gmail.com>
2020-02-07 15:26:55 -0800

    Debug

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ function Page () {
     obj: {}, // workspace object
     homologTable: HomologTable(),
     setUpa (upa) {
-      this.pendingInput = false;
-      this.homologTable.fetch(upa);
-      this._render();
+      this.pendingInput = false
+      this.homologTable.fetch(upa)
+      this._render()
     },
     view
   })
@@ -60,6 +60,7 @@ function receiveMessage (ev) {
   if (!data) {
     return
   }
+  console.log('Received message:', data)
   try {
     data = JSON.parse(ev.data)
   } catch (e) {
@@ -78,6 +79,7 @@ function receiveMessage (ev) {
 // Handle post message methods
 window._messageHandlers = {
   setConfig: function ({ config }) {
+    console.log('Setting config:', config)
     window._env = Object.assign(window._env, config)
     if (config.upa) {
       document._page.setUpa(config.upa)
